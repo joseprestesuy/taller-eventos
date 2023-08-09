9bfe0194ef0f112e3aa9c89ef23335ad34ed2aef
Sinho_107 <jose.prestes107@gmail.com>
2023-08-09 20:31:40 -0300

    function div

---
+++ event.js
@@ -0,0 +1,9 @@
+//document.addEventListener("DOMContentLoaded", );
+
+ function saludar(){
+    alert('Hola! Soy el div');
+ }
+
+ let divSaludar= document.getElementById("idiv1");
+
+ idiv1.addEventListener('click', saludar);
