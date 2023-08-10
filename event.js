d269bbf77dfe9406f230d955dfe62b57c24b35cd
Sinho_107 <jose.prestes107@gmail.com>
2023-08-09 21:18:02 -0300

    solucion-click

--- event.js
+++ event.js
@@ -7,3 +7,8 @@
  let divSaludar= document.getElementById("idiv1");
 
  divSaludar.addEventListener('click', saludar);
+
+ let btnSaludar= document.getElementById("idbtnSaludar")
+ btnSaludar.addEventListener("click", function(event) {
+   event.stopPropagation(); 
+});
